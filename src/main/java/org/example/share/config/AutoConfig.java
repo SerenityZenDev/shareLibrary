b9acd57ae9f6673a8afbcf4c11a39474a83787f8
SerenityZenDev <tjrqo2000@gmail.com>
2024-04-03 12:22:40 +0900

    :sparkles: 공유 라이브러리 생성 이후 적용 테스트 완료

---
+++ src/main/java/org/example/share/config/AutoConfig.java
@@ -0,0 +1,10 @@
+package org.example.share.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.example.share")
+public class AutoConfig {
+
+}
